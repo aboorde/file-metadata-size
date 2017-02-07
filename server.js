6bff561e5d7cc18b46732ca578ca8ea6b79763dc
Andrew Boorde <andrew.boorde@gmail.com>
2017-02-07 07:04:00 +0000

    Write file size service

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ app.get('/', function(req, res) {
     res.render('index');
 })
 
+app.post('/file-size', function(req, res){
+    console.log(req.headers['content-length']);
+    res.send({size: req.headers['content-length']});
+});
+
+
 app.listen(process.env.PORT || 8080, function () {
   console.log('Example app listening on port 3000!')
 })
